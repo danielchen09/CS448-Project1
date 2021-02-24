095b4d5cefff58f51126b3df3284d52d26d41b31
Sidhartha Agrawal <64621557+agrawa96-sid@users.noreply.github.com>
2021-02-24 10:01:29 -0500

    Update BufferMgr.java

--- src/simpledb/buffer/BufferMgr.java
+++ src/simpledb/buffer/BufferMgr.java
@@ -1,136 +1,72 @@
 package simpledb.buffer;
-
 import simpledb.file.*;
 import simpledb.log.LogMgr;
 
-/**
- * Manages the pinning and unpinning of buffers to blocks.
- * @author Edward Sciore
- *
- */
-public class BufferMgr {
-   protected Buffer[] bufferpool;
-   protected int numAvailable;
-   protected static final long MAX_TIME = 10000; // 10 seconds
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-   public BufferMgr() {
+public class BufferMgrBuffMap extends BufferMgr {
+    private HashMap<BlockId, Buffer> bufferMap = new HashMap<>();
+    private List<Buffer> unpinnedBlocks = new ArrayList<>();
+    private int numbuffs;
 
-   }
-   /**
-    * Creates a buffer manager having the specified number 
-    * of buffer slots.
-    * This constructor depends on a {@link FileMgr} and
-    * {@link simpledb.log.LogMgr LogMgr} object.
-    * @param numbuffs the number of buffer slots to allocate
-    */
-   public BufferMgr(FileMgr fm, LogMgr lm, int numbuffs) {
-      bufferpool = new Buffer[numbuffs];
-      numAvailable = numbuffs;
-      for (int i=0; i<numbuffs; i++)
-         bufferpool[i] = new Buffer(fm, lm);
-   }
-   
-   /**
-    * Returns the number of available (i.e. unpinned) buffers.
-    * @return the number of available buffers
-    */
-   public synchronized int available() {
-      return numAvailable;
-   }
-   
-   /**
-    * Flushes the dirty buffers modified by the specified transaction.
-    * @param txnum the transaction's id number
-    */
-   public synchronized void flushAll(int txnum) {
-      for (Buffer buff : bufferpool)
-         if (buff.modifyingTx() == txnum)
-         buff.flush();
-   }
-   
-   
-   /**
-    * Unpins the specified data buffer. If its pin count
-    * goes to zero, then notify any waiting threads.
-    * @param buff the buffer to be unpinned
-    */
-   public synchronized void unpin(Buffer buff) {
-      buff.unpin();
-      if (!buff.isPinned()) {
-         numAvailable++;
-         notifyAll();
-      }
-   }
-   
-   /**
-    * Pins a buffer to the specified block, potentially
-    * waiting until a buffer becomes available.
-    * If no buffer becomes available within a fixed 
-    * time period, then a {@link BufferAbortException} is thrown.
-    * @param blk a reference to a disk block
-    * @return the buffer pinned to that block
-    */
-   public synchronized Buffer pin(BlockId blk) {
-      try {
-         long timestamp = System.currentTimeMillis();
-         Buffer buff = tryToPin(blk);
-//         while (buff == null && !waitingTooLong(timestamp)) {
-//            wait(MAX_TIME);
-//            buff = tryToPin(blk);
-//         }
-         if (buff == null)
-            throw new BufferAbortException();
-         return buff;
-      }
-      catch(Exception e) {
-         throw new BufferAbortException();
-      }
-   }  
-   
-   protected boolean waitingTooLong(long starttime) {
-      return System.currentTimeMillis() - starttime > MAX_TIME;
-   }
-   
-   /**
-    * Tries to pin a buffer to the specified block. 
-    * If there is already a buffer assigned to that block
-    * then that buffer is used;  
-    * otherwise, an unpinned buffer from the pool is chosen.
-    * Returns a null value if there are no available buffers.
-    * @param blk a reference to a disk block
-    * @return the pinned buffer
-    */
-   protected Buffer tryToPin(BlockId blk) {
-      Buffer buff = findExistingBuffer(blk);
-      if (buff == null) {
-         buff = chooseUnpinnedBuffer();
-         if (buff == null)
+    public BufferMgrBuffMap(FileMgr fm, LogMgr lm, int numbuffs) {
+        this.numAvailable = numbuffs;
+        this.numbuffs = numbuffs;
+        for (int i = 0; i < numbuffs; i++)
+            unpinnedBlocks.add(new Buffer(fm, lm));
+    }
+
+    @Override
+    protected Buffer tryToPin(BlockId blk) {
+        Buffer buff = findExistingBuffer(blk);
+        if (buff == null) {
+            buff = chooseUnpinnedBuffer();
+            if (buff == null)
+                return null;
+            if (buff.block() != blk) {
+                bufferMap.remove(buff.block());
+            }
+            buff.assignToBlock(blk);
+            bufferMap.put(blk, buff);
+        }
+        if (!buff.isPinned()) {
+            unpinnedBlocks.remove(buff);
+            numAvailable--;
+        }
+        buff.pin();
+        return buff;
+    }
+
+    @Override
+    protected Buffer findExistingBuffer(BlockId blk) {
+        if (bufferMap.containsKey(blk))
+            if (bufferMap.get(blk) != null)
+                return bufferMap.get(blk);
             return null;
-         buff.assignToBlock(blk);
-      }
-      if (!buff.isPinned())
-         numAvailable--;
-      buff.pin();
-      return buff;
-   }
-   
-   protected Buffer findExistingBuffer(BlockId blk) {
-      for (Buffer buff : bufferpool) {
-         BlockId b = buff.block();
-         if (b != null && b.equals(blk))
-            return buff;
-      }
-      return null;
-   }
-   
-   protected Buffer chooseUnpinnedBuffer() {
-      for (Buffer buff : bufferpool)
-         if (!buff.isPinned())
-         return buff;
-      return null;
-   }
+    }
 
-   public int getNumBuffs() {
-      return bufferpool.length;
-   }
+    @Override
+    protected Buffer chooseUnpinnedBuffer() {
+        if (unpinnedBlocks.size() > 0) {
+            return unpinnedBlocks.remove(0);
+        }
+        return null;
+    }
+
+    @Override
+    public int getNumBuffs() {
+        return numbuffs;
+    }
+
+    @Override
+    public synchronized void unpin(Buffer buff) {
+        buff.unpin();
+        if (!buff.isPinned()) {
+            numAvailable++;
+            unpinnedBlocks.add(buff);
+            notifyAll();
+        }
+    }
 }
