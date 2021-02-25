8bc18624c8eb7020651680958a7fab05c9792ec8
Yukyung Kate Lee <katelee0421@gmail.com>
2021-02-25 04:26:38 -0500

    BufferMgrHashTable(2)

--- src/simpledb/buffer/BufferMgrHashTable.java
+++ src/simpledb/buffer/BufferMgrHashTable.java
@@ -22,23 +22,33 @@ public class BufferMgrHashTable extends BufferMgr{
 
    public BufferMgrHashTable(FileMgr fm, LogMgr lm, int numbuffs) {
       super(fm, lm, numbuffs);
+      bufferpool = new Buffer[numbuffs];
       buffHash = new HashMap<>(numbuffs);
+      for(int i = 0; i < numbuffs; i++){
+         bufferpool[i] = new Buffer(fm, lm);
+         //at the same time when we put buffer into bufferpool, we store key value pair into the hashmap
+         buffHash.put(bufferpool[i].block(), i);
+      }
    }
 
-   //implement a hash table for the bufferpool, make searching in bufferpool quicker, O(1)
+   //implement a hash table for the bufferpool, make searching in bufferpool quicker
    //key that maps to a buffer, key - BlockId, value - index in the bufferpool
 
    protected Buffer findExistingBuffer(BlockId blk){
-      Integer i = buffHash.get(blk); //getting the index of bufferpool
-      return bufferpool[i];
+      //getting the index of bufferpool through buffHash.get(blk)
+      //get put the value (index) to get to certain place in the array
+      if (buffHash.get(blk) == null || blk == null){
+         return null;
+      }
+      return bufferpool[buffHash.get(blk)];
    }
 
-   protected Buffer chooseUnpinnedBuffer(){
-      for (Integer i: buffHash.values()){
-         if(!bufferpool[i].isPinned()){
-            return bufferpool[i];
-         }
-      }
-      return null;
-   }
+//   protected Buffer chooseUnpinnedBuffer(){
+//      for (Integer i: buffHash.values()){
+//         if(!bufferpool[i].isPinned()){
+//            return bufferpool[i];
+//         }
+//      }
+//      return null;
+//   }
 }
